42af6a1f13b91b91807af0020adb76693506ee5b
joellepanza <joellepanza@192.168.0.4>
2013-12-04 01:57:55 -0700

    created login and signup

--- CardGames/src/com/example/cardgames/MainActivity.java
+++ CardGames/src/com/example/cardgames/MainActivity.java
@@ -6,8 +6,10 @@ import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
@@ -15,12 +17,17 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	Button signup;
+	Button loginbutton;
 	EditText username;
 	EditText password;
+	String usernametxt;
+    String passwordtxt;
 	Context context;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +39,87 @@ public class MainActivity extends Activity {
 		//the arguments here are used to identify this app with the Parse service
 		Parse.initialize(this, "rcoJm1GGgUI9wgN19GDGBk8qgbsHD3buV3MiquBp", "lUYmJQ1pQS3MrOMTrmycEKS8jRCg0Vzd45HYxLJz"); 
 		
-		User user = new User("e456","IamHere","skater85234@hotmail.com");
-		context = this;
-		user.signUp(); //method can be used to create a username and password on Parse
-	
+		//User user = new User("e456","IamHere","skater85234@hotmail.com");
+		//context = this;
+		//user.signUp(); //method can be used to create a username and password on Parse
+		loginbutton = (Button) findViewById(R.id.stayButton);
+		signup = (Button) findViewById(R.id.signup);
+		
+        // Login Button Click Listener
+        loginbutton.setOnClickListener(new OnClickListener() {
+ 
+            public void onClick(View v) {
+                // Retrieve the text entered from the EditText
+                usernametxt = username.getText().toString();
+                passwordtxt = password.getText().toString();
+                // Force user to fill up the form
+                if (usernametxt.equals("") && passwordtxt.equals("")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Please complete the login form",
+                            Toast.LENGTH_LONG).show();
+                } 
+                else
+                {
+                // Send data to Parse.com for verification
+                ParseUser.logInInBackground(usernametxt, passwordtxt,
+                        new LogInCallback() {
+                            public void done(ParseUser user, ParseException e) {
+                                if (user != null) {
+                                    // If user exist and authenticated, send user to Welcome.class
+                                	Intent i = new Intent(MainActivity.this, BlackJackActivity.class);
+             				       startActivity(i);
+                                    
+                                    Toast.makeText(getApplicationContext(),
+                                            "Successfully Logged in",
+                                            Toast.LENGTH_LONG).show();
+                                    finish();
+                                } else {
+                                    Toast.makeText(
+                                            getApplicationContext(),
+                                            "No such user exist, please signup",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+                }
+            }
+        });		
+		// Sign up Button Click Listener
+	    signup.setOnClickListener(new OnClickListener() {
+        public void onClick(View v) {
+            // Retrieve the text entered from the EditText
+            usernametxt = username.getText().toString();
+            passwordtxt = password.getText().toString();
+
+            // Force user to fill up the form
+            if (usernametxt.equals("") && passwordtxt.equals("")) {
+                Toast.makeText(getApplicationContext(),
+                        "Please complete the sign up form",
+                        Toast.LENGTH_LONG).show();
+            } 
+            else 
+            {
+                // Save new user data into Parse.com Data Storage
+                ParseUser user = new ParseUser();
+                user.setUsername(usernametxt);
+                user.setPassword(passwordtxt);
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            // Show a simple Toast message upon successful registration
+                            Toast.makeText(getApplicationContext(),
+                                    "Successfully Signed up, please log in.",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    "Username is already registered", Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    }
+                });
+            }
+           }
+	    });
 	}
 
 	@Override
@@ -44,7 +128,7 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	public void login(View v){
+	/*public void login(View v){
 		ParseUser.logInInBackground(username.getText().toString(), password.getText().toString(), new LogInCallback() {
 			  @Override
 			@SuppressLint("ShowToast")
@@ -63,16 +147,12 @@ public class MainActivity extends Activity {
 			    }
 			  }
 			});
-		
-	}
-	public void signUp(View v){
-		Intent i = new Intent(MainActivity.this, User.class);
-	       startActivity(i);
-	}
-	
+	}*/
 	public void createUser(View v){
 		Intent i = new Intent(MainActivity.this, HighScoreActivity.class);
 	       startActivity(i);
 	}
-
+	
 }
+
+ 
